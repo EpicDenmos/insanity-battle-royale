4664189ef5198f2ded346ac86ed7efa64b65f7dc
Guillaume (Epic) Escanyé <guillaume.escanye@gmail.com>
2020-02-13 22:38:04 +0100

    Init

---
+++ scenes/login.ts
@@ -0,0 +1,23 @@
+/**
+ * @author       Digitsensitive <digit.sensitivee@gmail.com>
+ * @copyright    2018 - 2019 digitsensitive
+ * @license      {@link https://github.com/digitsensitive/phaser3-typescript/blob/master/LICENSE.md | MIT License}
+ */
+
+export class Login extends Phaser.Scene {
+  private phaserSprite: Phaser.GameObjects.Sprite;
+
+  constructor() {
+    super({
+      key: "Login"
+    });
+  }
+
+  preload(): void {
+    
+  }
+
+  create(): void {
+    
+  }
+}
